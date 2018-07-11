a8a42a00a7913f8c2780faddb928f67464003e12
stuartsmyth <40466017+stuartsmyth@users.noreply.github.com>
2018-07-11 12:25:19 +0100

    Fast Adc read sketch for arduino
    puts the adc of arduino due into freerunn mode. has average conversion time of 1.5 micro secounds to reads up to around 600 kHz

---
+++ Fast_ADC/Fast_ADC.ino
@@ -0,0 +1,39 @@
+unsigned long start_time;
+unsigned long stop_time;
+unsigned long values[1000];
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  ADC->ADC_MR |= 0x80;  //set free running mode on ADC
+  ADC->ADC_CHER = 0x80; //enable ADC on pin A0
+}
+
+void loop() {
+  unsigned int i;
+
+  start_time = micros();
+  for (i = 0; i < 2000; i++) {
+    while ((ADC->ADC_ISR & 0x80) == 0); // wait for conversion
+    values[i] = ADC->ADC_CDR[7]; //get values
+  }
+  stop_time = micros();
+
+  Serial.print("Total time: ");
+  Serial.println(stop_time - start_time);
+  Serial.print("Average time per conversion: ");
+  Serial.println((float)(stop_time - start_time) / 1000);
+
+  Serial.println("Values: ");
+  for (i = 0; i < 1000; i++) {
+    Serial.println(values[i]);
+  }
+
+  delay(2000);
+}
+
+
+
+
+
+
