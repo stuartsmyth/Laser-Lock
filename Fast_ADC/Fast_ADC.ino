278ffef12a1713d4b93d0adf980fd794bb13c2f7
stuartsmyth <40466017+stuartsmyth@users.noreply.github.com>
2018-07-18 10:04:26 +0100

    Added both an live viewing script and a function that set up serial comsbetween matlab workspace and arduino

--- Fast_ADC/Fast_ADC.ino
+++ Fast_ADC/Fast_ADC.ino
@@ -1,10 +1,10 @@
 unsigned long start_time;
 unsigned long stop_time;
-unsigned long values[1000];
+unsigned long values[10000];
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(9600);
+  Serial.begin(115200);
   ADC->ADC_MR |= 0x80;  //set free running mode on ADC
   ADC->ADC_CHER = 0x80; //enable ADC on pin A0
 }
@@ -13,23 +13,23 @@ void loop() {
   unsigned int i;
 
   start_time = micros();
-  for (i = 0; i < 2000; i++) {
+  for (i = 0; i < 10000; i++) {
     while ((ADC->ADC_ISR & 0x80) == 0); // wait for conversion
     values[i] = ADC->ADC_CDR[7]; //get values
   }
   stop_time = micros();
 
-  Serial.print("Total time: ");
-  Serial.println(stop_time - start_time);
-  Serial.print("Average time per conversion: ");
-  Serial.println((float)(stop_time - start_time) / 1000);
+  //Serial.print("Total time: ");
+  //Serial.println(stop_time - start_time);
+  //Serial.print("Average time per conversion: ");
+  //Serial.println((float)(stop_time - start_time) / 10000);
 
-  Serial.println("Values: ");
+  //Serial.println("Values: ");
   for (i = 0; i < 1000; i++) {
     Serial.println(values[i]);
   }
 
-  delay(2000);
+  delay(10000);
 }
 
 
